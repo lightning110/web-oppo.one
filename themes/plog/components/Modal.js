8150a2d9f078361ae300beec35cc20a469dd0439
lightning <admin@chenxyz.cn>
2023-10-19 19:33:17 +0800

    v1.68

--- themes/plog/components/Modal.js
+++ themes/plog/components/Modal.js
@@ -108,8 +108,9 @@ export default function Modal(props) {
                                             </div>
                                         )}
                                     </div>
+                                    {/* location='https://blog.csdn.net/xijinno1?type=blog */}
                                     <Link href={`${BLOG.SUB_PATH}/${modalContent.slug}`}>
-                                    <div className='z-10 absolute hover:opacity-50 opacity-0 duration-200 transition-opacity w-full top-0 left-0 px-4 h-full items-center flex justify-between'>
+                                    <div  onClick={`location='${BLOG.SUB_PATH}/${modalContent.slug}'`} className='z-10 absolute hover:opacity-50 opacity-0 duration-200 transition-opacity w-full top-0 left-0 px-4 h-full items-center flex justify-between'>
                                         <a class="z-top" onClick={prev}><ChevronLeft className='cursor-pointer w-24 h-32 hover:opacity-100 stroke-white stroke-1 scale-y-150' /></a>
                                         <a class="z-top" onClick={next}><ChevronRight className='cursor-pointer w-24 h-32 hover:opacity-100 stroke-white stroke-1 scale-y-150' /></a>
                                     </div>
